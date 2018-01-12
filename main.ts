fc995e0f81b8dc9df9358aa0f1ff51eb320b8e51
maowu <mao@mutienliao.com>
2018-01-12 21:39:53 +0900

    update fixed

---
+++ main.ts
@@ -0,0 +1,116 @@
+//% weight=0 color=#3CB371 icon="\uf0ad"
+namespace GameRemoteConsole{
+    let btnA = 0
+    let btnB = 0
+    let P0 = 0
+    let P1 = 0
+    let P2 = 0
+    let lastbtnA = 0
+    let lastbtnB = 0
+    let lastP0 = 0
+    let lastP1 = 0
+    let lastP2 = 0
+    let imu_tiemr = 0;
+
+
+    /**
+    * 初始，設定radio群組
+    */
+    //% blockId="ConsoleInit" block="console init|id(1-10) %group_id"
+    //% blockGap=20 weight=75
+    //% group_id.min=1 group_id.max=10
+    export funciton ConsoleInit(group_id: number) {
+        radio.setGroup(group_id)
+        radio.setTransmitSerialNumber(true)
+        radio.setTransmitPower(7)
+    }
+
+    /**
+    * 搖桿的執行功能，捕捉A/B按鍵以及加速計的X軸
+    */
+    //% blockId="ConsoleExcue" block="console excue"
+    //% blockGap=20 weight=75
+    export function ConsoleExcue(): void {
+        if (input.buttonIsPressed(Button.A)) {
+            btnA = 1
+            basic.showString("A")
+            radio.sendValue("btnA", 1)
+        }else {
+            btnA = 0
+        }
+        if (input.buttonIsPressed(Button.B)) {
+            btnB = 1
+            basic.showString("B")
+            radio.sendValue("btnB", 1)
+        }else {
+            btnB = 0
+        }
+        if (input.pinIsPressed(TouchPin.P0)) {
+            P0 = 1
+            basic.showString("0")
+            radio.sendValue("P0", 1)
+        }else {
+            P0 = 0
+        }
+        if (input.pinIsPressed(TouchPin.P1)) {
+            P1 = 1
+            basic.showString("1")
+            radio.sendValue("P1", 1)
+        }else {
+            P1 = 0
+        }
+        if (input.pinIsPressed(TouchPin.P2)) {
+            P2 = 1
+            basic.showString("2")
+            radio.sendValue("P2", 1)
+        }else {
+            P2 = 0
+        }
+        if (btnA!=lastbtnA) {
+            if(btnA > 0) {
+                
+            }else {
+                basic.clearScreen()
+            }
+            lastbtnA = btnA
+        }
+        if (btnB!=lastbtnB) {
+            if(btnB > 0) {
+                
+            }else {
+                basic.clearScreen()
+            }
+            lastbtnB = btnB
+        }
+        if (P0!=lastP0) {
+            if(P0 > 0) {
+                
+            }else {
+                basic.clearScreen()
+            }
+            lastP0 = P0
+        }
+        if (P1!=lastP1) {
+            if(P1 > 0) {
+                
+            }else {
+                basic.clearScreen()
+            }
+            lastP1 = P1
+        }
+        if (P2!=lastP2) {
+            if(P2 > 0) {
+                
+            }else {
+                basic.clearScreen()
+            }
+            lastP2 = P2
+        }
+        if(input.runningTime()-imu_tiemr > 100) {
+            radio.sendValue("pitch", input.rotation(Rotation.Pitch))
+            radio.sendValue("roll", input.rotation(Rotation.Roll))
+            imu_tiemr = input.runningTime()
+        }
+    }
+
+}
