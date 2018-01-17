37c80371952400b95d09be64106a1e81b3a5ddf3
maowu <mao@mutienliao.com>
2018-01-17 13:54:15 +0900

    updated

--- main.ts
+++ main.ts
@@ -195,7 +195,7 @@ namespace GameRemoteConsole{
     */
     //% blockId="RemoteRadioDatasHandle" block="get datas from remote|SerialOut %isoutput"
     //% blockGap=20 weight=80
-    export function RemoteRadioDatasHandle(isoutput: SetYesNo): void {
+    export function RemoteRadioDatasHandle(isoutput: number): void {
         //let t_output = isoutput
         let t_output = 0 
         if(isoutput>0) {
