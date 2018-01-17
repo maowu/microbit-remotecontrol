b033e24edc779cc8c8bf9cd61725db73be6eeb76
maowu <mao@mutienliao.com>
2018-01-17 13:13:23 +0900

    updated

--- main.ts
+++ main.ts
@@ -197,12 +197,7 @@ namespace GameRemoteConsole{
     //% blockGap=20 weight=80
     export function RemoteRadioDatasHandle(msg_name: String, msg_value: number, isoutput: number): void {
         let t_output = isoutput
-        /*
-        switch(isoutput) {
-            case SetYesNo.NO: t_output = 0; break;
-            case SetYesNo.YES: t_output = 1; break;
-        }
-        */
+        
         if (msg_name.compare("btnA") == 0) {
             if (msg_value > 0) {
                 basic.showString("A")
